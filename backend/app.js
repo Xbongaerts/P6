d0b2c43f068e65b63e864d60f54e75b46d77e3a3
Xbongaerts <okrim77@gmail.com>
2023-01-12 13:44:45 +0100

    first commit

---
+++ backend/app.js
@@ -0,0 +1,54 @@
+////////// contient de l'application //////////
+
+//importations
+require('dotenv').config(); //charge les variables d'environnement
+const express = require('express'); //framework node.js
+const mongoose = require('mongoose'); //facilite interactions avec DB MongoDB
+const path = require('path'); //donne accès au chemin de notre système de fichier
+const rateLimit = require('express-rate-limit'); //limite les requêtes par IP
+const helmet = require('helmet'); //définit divers en-têtes HTTP sécurisées
+const mongoSanitize = require('express-mongo-sanitize'); //protège des attaques par injection NoSQL(MongoDB)
+const cors = require('cors'); //permet de protéger les en-têtes
+
+
+//création de notre application express
+const app = express();
+
+app.use(helmet());
+
+//connexion à bdd MongoDB via mongoose
+mongoose.connect('mongodb+srv://okrim77:psg777SNT@cluster0.opsyyku.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+//cors
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+//permet de protéger les en-têtes 
+app.use(cors());
+
+//rend le corps des requêtes json (de tt types) => en objet JS utilisable -- anciennement body-parser
+app.use(express.json());
+
+app.use(mongoSanitize());
+
+//gestion des fichiers images
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+//importe les routes
+const userRoutes = require('./routes/user'); 
+const saucesRoutes = require('./routes/sauces');
+
+//enregistre les routeurs dans l'application
+app.use('/api/auth', userRoutes);
+app.use('/api/sauces', saucesRoutes);
+
+//exporte l'application
+module.exports = app;
---
+++ backend/routes/user.js
@@ -0,0 +1,19 @@
+////////// contient logique de routing pour user //////////
+
+//importe Express
+const express = require("express");
+
+//création d'un routeur Express
+const router = express.Router();
+
+//importe la logique métier de sauce
+const userCtrl = require("../controllers/user");
+//fonction qui permet de vérifier si l'email est correct
+const emailValidation = require("../middleware/emailValidation");
+
+//routes disponibles dans l'application avec leur nom de fonction (avec une sémantique qui permet de savoir ce qu'elles font)
+router.post("/signup", emailValidation, userCtrl.signup);
+router.post("/login", userCtrl.login);
+
+//exporte le routeur Express
+module.exports = router;
